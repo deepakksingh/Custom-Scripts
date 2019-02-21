3a7401bf458ee71751269fbd7e310a6e07e2f3f9
deepakksingh <deepak.singh@research.iiit.ac.in>
2019-02-21 23:40:09 +0530

    added functions to send email and also performed ssmtp setup

--- ipResponder/main.py
+++ ipResponder/main.py
@@ -6,12 +6,16 @@ Created on Wed Feb 20 23:05:16 2019
 @author: deepak
 """
 import subprocess
+import os
 import ipResponderConfiguration as cfg
 
 
 def getHostIP():
     retVal = subprocess.check_output(['hostname','-I'])
-    return retVal
+    if(retVal == ' '):
+        retVal = cfg.HOSTNOTCONNECTED
+        
+    return retVal.decode('utf-8')
 
 #IPVal = getHostIP().decode('utf-8')
 #print(IPVal)
@@ -32,4 +36,36 @@ def runnerFunc():
     
     if currIPVal != storedIPVal:
         pass
+
+def getDestinationAddress():
+    return cfg.DESTINATION_EMAIL
         
+def sender():
+    currIPVal = getHostIP()
+    hostnameVal = subprocess.check_output(['hostname'])
+    hostnameVal = hostnameVal.decode("utf-8")
+    #prepare and send the mail
+    destEmail = getDestinationAddress()
+    
+    emailContent = 'The current IP of '+hostnameVal+' is '+currIPVal
+    
+    emailObj = {}
+    emailObj['destEmail']=destEmail
+    emailObj['emailContent']=emailContent
+    
+    sendEmail(emailObj)
+    
+def createFile(emailObj):
+    f = open('./mailData.txt','w')
+    f.write(emailObj['emailContent'])    
+    f.close()
+ 
+def sendEmail(emailObj):
+    
+    createFile(emailObj)
+    #subprocess.call(['ssmtp',emailObj['destEmail'],'<','mailData.txt'])
+    os.system('ssmtp '+emailObj['destEmail']+' < ./mailData.txt')
+    
+#sender()
+
+    
